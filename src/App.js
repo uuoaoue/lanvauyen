3b907f9b4605600eaa2bd2bfef1c2f697bd8d291
lannguyen <nguyentruongngoclan@gmail.com>
2022-08-02 15:18:07 -0700

    Change to react app
    Title

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Tụi mình đang build website, đợi xíu nhé 
+      </header>
+    </div>
+  );
+}
+
+export default App;
